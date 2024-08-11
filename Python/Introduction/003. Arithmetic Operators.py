e6c44ea1761e0a8227bff4424e249d717c3aadc5
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-08-10 22:42:12 -0600

    Creación de carpeta Python

---
+++ Python/Introduction/003. Arithmetic Operators.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Score 10
+
+def arithmetic_operators(a, b):
+  print(a + b)
+  print(a - b)
+  print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    arithmetic_operators(a, b)
---
+++ Python/Introduction/004. Python: Division.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Score 10
+
+def Int_float_division(a, b):
+  print(a // b)
+  print(a / b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    Int_float_division(a, b)
