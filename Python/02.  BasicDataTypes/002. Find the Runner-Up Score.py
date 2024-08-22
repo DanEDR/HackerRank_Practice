5a1531f8fbc650c4a3bbd814c83fe572b553dde7
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-08-21 22:17:43 -0600

    se agregó el ejercicio 002

---
+++ Python/02.  BasicDataTypes/002. Find the Runner-Up Score.py
@@ -0,0 +1,16 @@
+
+
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+# Score 10
+
+def runner_up_score(n, arr):
+    max_score = max(arr)
+    arr = list(set(arr))
+    arr.remove(max_score)
+    return print(max(arr))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    runner_up_score(n, arr)
