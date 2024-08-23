226452a0a5f24e357ff003de04d741461b9f3914
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-08-22 21:54:14 -0600

    actualización ejercicio 002

--- Python/02.  BasicDataTypes/002. Find the Runner-Up Score.py
+++ Python/02.  BasicDataTypes/002. Find the Runner-Up Score.py
@@ -7,10 +7,10 @@ def runner_up_score(n, arr):
     max_score = max(arr)
     arr = list(set(arr))
     arr.remove(max_score)
-    return print(max(arr))
+    return max(arr)
 
 
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    runner_up_score(n, arr)
+    print(runner_up_score(n, arr))
