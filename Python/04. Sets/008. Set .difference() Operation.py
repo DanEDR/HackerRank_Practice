aeaa9ae7a5c70994d7643f536687ba9568680e2f
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-09-16 22:36:03 -0600

    Se ha agregado el ejercicio 008. Set .difference() Operation

---
+++ Python/04. Sets/008. Set .difference() Operation.py
@@ -0,0 +1,16 @@
+
+
+# https://www.hackerrank.com/challenges/py-set-difference-operation/problem?isFullScreen=true
+# Score 10
+
+def count_unique_students():
+  number_students_english = int(input())
+  students_english = set(map(int, input().split()))
+
+  number_students_french = int(input())
+  students_french = set(map(int, input().split()))
+
+  total_number_students = students_english.intersection(students_french)
+  return len(total_number_students)
+
+print(count_unique_students())
