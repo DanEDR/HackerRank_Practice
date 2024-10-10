70d7ceb64f6917891195c47cd83b8239d51cc4ef
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-10-09 22:49:52 -0600

    Se agregó el ejercicio 006. Power - Mod Power

---
+++ Python/05. Math/006. Power - Mod Power.py
@@ -0,0 +1,10 @@
+
+
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+# Score 10
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b), pow(a,b,m), sep='\n')
