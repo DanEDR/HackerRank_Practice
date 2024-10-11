b54e84330d8dfa42d3ead02b0cf1bbe341ff7cfd
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-10-10 22:12:52 -0600

    Se agregó el ejercicio 007. Integers Come In All Sizes

---
+++ Python/05. Math/007. Integers Come In All Sizes.py
@@ -0,0 +1,8 @@
+
+
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+# Score 10
+
+a, b, c, d = (int(input()) for _ in range(4))
+
+print(a ** b + c ** d)
