da5fad9b9e625341b991f536d44939e3e5529b3f
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-11-03 18:38:08 -0600

    Se agregó el ejercicio 002. Input()

---
+++ Python/11. Built-Ins/002. Input().py
@@ -0,0 +1,12 @@
+
+
+# https://www.hackerrank.com/challenges/input/problem?isFullScreen=true
+# Score 20
+
+polynomial = lambda x: eval(function)
+
+if __name__ == '__main__':
+  x, k = map(int, input().split())
+  function = input()
+  
+  print(polynomial(x) == k)
