641c41df6fbeb282c7e4750a47a5098854c5e3b3
DanEDR <dann.dominguez@aulavirtual.umar.mx>
2024-11-24 23:27:12 -0600

    Se agregó el ejercicio 014. Validating UID

---
+++ Python/13. Regex and Parsing/014. Validating UID.py
@@ -0,0 +1,19 @@
+
+
+# https://www.hackerrank.com/challenges/validating-uid/problem?isFullScreen=true
+# Score 30
+
+import re 
+
+pattern = re.compile(r'^(?!.*(.).*\1)[A-Za-z0-9]{10}$')
+
+
+def is_valid_UID(string):
+  return 'Valid' if pattern.match(string) else 'Invalid'
+  
+if __name__ == '__main__':
+  num_of_test = int(input())
+  
+  for _ in range(num_of_test):
+    UID = input()
+    print(is_valid_UID(UID))
